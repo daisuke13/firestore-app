1e9dbc6b8a2773269a5b2c6d33283b602a66d8f3
daisuke13 <people.in.the.box.13@gmail.com>
2018-02-15 23:11:00 +0900

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyA3j6_dhDocfMTaJ6CSXQFX2I8m2LdTqWo",
+  authDomain: "sanguine-rhythm-171508.firebaseapp.com",
+  databaseURL: "https://sanguine-rhythm-171508.firebaseio.com",
+  projectId: "sanguine-rhythm-171508",
+  storageBucket: "sanguine-rhythm-171508.appspot.com",
+  messagingSenderId: "732548066979"
+}
